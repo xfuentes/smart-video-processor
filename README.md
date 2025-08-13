fad0cde252f71fcc9fceceed266b7736b2f396c5
Xavier Fuentes <xfuentes-dev@hotmail.com>
2025-08-13 20:09:15 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Smart Video Processor
+
+An Electron application with React and TypeScript
+
+## Recommended IDE Setup
+
+- [WebStorm](https://www.jetbrains.com/webstorm/) + [TSLint](https://plugins.jetbrains.com/plugin/17563-tslint) + [Prettier](https://plugins.jetbrains.com/plugin/10456-prettier)
+
+## Project Setup
+
+### Install
+
+```bash
+$ npm install
+```
+
+### Development
+
+```bash
+$ npm run dev
+```
+
+### Build
+
+```bash
+# For windows
+$ npm run build:win
+
+# For macOS
+$ npm run build:mac
+
+# For Linux
+$ npm run build:linux
+```
---
+++ src/preload/index.ts
@@ -0,0 +1,26 @@
+import { contextBridge } from 'electron'
+import { electronAPI } from '@electron-toolkit/preload'
+import { ipcRenderer } from 'electron/renderer'
+
+// Custom APIs for renderer
+const api = {
+  version: () => ipcRenderer.invoke('retrieve:version')
+}
+
+// Use `contextBridge` APIs to expose Electron APIs to
+// renderer only if context isolation is enabled, otherwise
+// just add to the DOM global.
+if (process.contextIsolated) {
+  console.log('isolated')
+  try {
+    contextBridge.exposeInMainWorld('electron', electronAPI)
+    contextBridge.exposeInMainWorld('api', api)
+  } catch (error) {
+    console.error(error)
+  }
+} else {
+  // @ts-ignore (define in dts)
+  window.electron = electronAPI
+  // @ts-ignore (define in dts)
+  window.api = api
+}
